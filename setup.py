5321d3e72e391007ef53f0035d69ea3d279897d7
juliandomeneghini <73919470+juliandomeneghini@users.noreply.github.com>
2023-06-08 21:51:58 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,22 +8,9 @@ with open("requirements.txt") as f:
 
 setup(name='image_processing-test',
     version='0.0.9',
-    license = 'MIT License'
-    author='Julian Domeneghini',
-    author_email='julian.domeneghini@gmail.com'
-    from setuptools import setup, find_packages
-
-with open("README.md", "r") as f:
-    page_description = f.read()
-
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
-setup(name='image_processing-test',
-    version='0.0.9',
     license = 'MIT License',
     author='Julian Domeneghini',
-    author_email='julian.domeneghini@gmail.com'
+    author_email='julian.domeneghini@gmail.com',
 	description = 'Test version - Image processing. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it\'s from class of the Bootcamp developer full stack Python. E-mail: karinatkato@gmail.com.'
     long_description=page_description,
     long_description_content_type="text/markdown",
